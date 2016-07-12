c5cfbc063d45f3c025aa71e19da329b8c1b53e7a
wvbe <wybe@x-54.com>
2016-07-12 17:15:34 +0200

    Dick.

--- scripts/log/LogInputComponent.js
+++ scripts/log/LogInputComponent.js
@@ -5,7 +5,10 @@ import React, {Component} from 'react';
 export default class LogInputComponent extends Component {
 	render() {
 		return (<oksee-log-message class='flex-row flex-gutter' data-connotation={this.props.connotation || 'log'}>
-			<oksee-log-prefix>{this.props.time.getTime()}</oksee-log-prefix>
+			<oksee-log-prefix class="flex-row flex-items-start">
+				<div className="content">{this.props.time.getTime()}</div>
+				<div className="arrow" />
+			</oksee-log-prefix>
 			<oksee-log-content>{this.props.children}</oksee-log-content>
 		</oksee-log-message>);
 	}
