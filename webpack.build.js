f625c293da357e10f4bed4b62a733b9b9ecdd840
wvbe <wybe@x-54.com>
2016-07-29 19:35:20 +0200

    refactored shit but "build" appears to have already been broken

---
+++ webpack.build.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-var, strict */
+
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = Object.assign({}, require('./webpack.config'), {
+	entry: './scripts/index',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
+	devtool: 'source-map',
+	plugins: [
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false
+			}
+		})
+	]
+});
---
+++ webpack.serve.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-var, strict */
+
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+
+var config = Object.assign({}, require('./webpack.config'), {
+	entry: [
+		'webpack-dev-server/client?http://localhost:5000',
+		'webpack/hot/dev-server',
+		'./scripts/index'
+	],
+	output: {
+		path: __dirname,
+		filename: 'bundle.js',
+		publicPath: '/static/'
+	},
+	devtool: 'eval-source-map',
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	]
+});
+
+new WebpackDevServer(webpack(config), {
+		publicPath: config.output.publicPath,
+		hot: true,
+		historyApiFallback: true
+	})
+	.listen(5000, 'localhost', function (err) {
+		if (err) {
+			console.log(err);
+		}
+		console.log('Listening at localhost:5000');
+	});
