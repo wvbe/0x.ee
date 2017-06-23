9a2ab81718d0319ee1644429687c056ad76071ae
wvbe <wybe@x-54.com>
2017-06-23 02:02:32 +0200

    Yes

---
+++ gh-pages.js
@@ -0,0 +1,13 @@
+const ghpages = require('gh-pages');
+
+console.log('Publishing');
+ghpages.publish(
+	'build',
+	{
+		repo: 'git@github.com:wvbe/wvbe.github.io.git',
+		branch: 'master'
+	},
+	err => err ?
+		console.log('  FAIL\n\n' + err.message) :
+		console.log('  OK')
+);
